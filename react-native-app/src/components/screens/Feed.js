d46335eef4a171146b9fd98359471de809de8f8f
Miguel <miguellemos95@gmail.com>
2018-08-20 04:37:44 -0300

    Added DELETE, and it was indeed added with ease. I would write ease in italics if I could.

--- react-native-app/src/components/screens/Feed.js
+++ react-native-app/src/components/screens/Feed.js
@@ -2,31 +2,75 @@ import React, { Component } from 'react';
 import {
   Text,
   View,
-  ScrollView
+  ScrollView,
+  StyleSheet
 } from 'react-native';
 import { ListItem } from 'react-native-elements';
-import { users } from '../config/data';
+
+import { Button } from 'react-native-elements';
+import Icon from 'react-native-vector-icons/FontAwesome';
 
 class Feed extends Component {
-  onLearnMore = (item) => {
-    console.log(this.props)
-    this.props.navigation.navigate('Details', { ...item });
-  };
+  onLearnMore = (item) => this.props.navigation.navigate('Details', { ...item })
 
   render = () => <View>
 
-    {this.props.screenProps.news_state &&
+    { this.props.screenProps.news_state &&
+      this.props.screenProps.news_state.news &&
+      this.props.screenProps.news_state.news.map &&
       <ScrollView>
           {this.props.screenProps.news_state.news.map((item, index) => (
-            <ListItem
-              key={"newsFeed " + index}
-              title={item.title}
-              onPress={() => this.onLearnMore(item)}
+            <View style={styles.row}
+              key={"newsFeedView " + index}>
+            <Button
+              key={"newsFeedTrashButton " + index}
+              icon={
+                <Icon
+                  name='trash'
+                  size={15}
+                  color='white'
+                  style={styles.trashButton}
+
+                />
+              }
+              title=""
+              onPress={()=>{
+                console.log("DELETING " + item._id)
+                this.props.screenProps.newsCRUD({
+                method: "DELETE",
+                url: item._id
+                })
+                this.props.screenProps.newsCRUD({
+                method: "GET"
+                })
+            }}
             />
+            <Text
+              style={styles.text}
+              key={"newsFeed " + index}
+              onPress={() => this.onLearnMore(item)}
+            >{item.title}</Text>
+            </View>
           ))}
       </ScrollView>
     }
   </View>
 }
+
+const styles = StyleSheet.create({
+  trashButton: {
+    padding: 2,
+    margin: 5,
+  },
+  row: {
+    flex: 1,
+    flexDirection: 'row',
+    padding: 2,
+    margin: 5,
+  },
+  text: {
+    marginLeft: 5,
+  },
+});
 
 export default Feed;
--- react-native-app/src/reducers/news.js
+++ react-native-app/src/reducers/news.js
@@ -1,31 +1,34 @@
 
 // I like my reducers to specify which actions belongs to them
-export const GET_NEWS = 'my-awesome-app/news/LOAD';
-export const GET_NEWS_SUCCESS = 'my-awesome-app/news/LOAD_SUCCESS';
-export const GET_NEWS_FAIL = 'my-awesome-app/news/LOAD_FAIL';
+export const CRUD_NEWS = 'my-awesome-app/news/LOAD';
+export const CRUD_NEWS_SUCCESS = 'my-awesome-app/news/LOAD_SUCCESS';
+export const CRUD_NEWS_FAIL = 'my-awesome-app/news/LOAD_FAIL';
 
 export default function news (state = { news: [] }, action) {
   switch (action.type) {
-    case GET_NEWS:
+    case CRUD_NEWS:
       return { ...state, loading: true };
-    case GET_NEWS_SUCCESS:
+    case CRUD_NEWS_SUCCESS:
       return { ...state, loading: false, news: action.payload.data };
-    case GET_NEWS_FAIL:
+    case CRUD_NEWS_FAIL:
       return { ...state, loading: false, error: action.payload.error };
     default:
       return state;
   }
 }
 
-export function CRUD({method, headers, data}) {
+export function CRUD({method, headers, data, url}) {
+  const sanitized_url = url || ""
+  const url_needs_fixing = sanitized_url[0] != "/" && sanitized_url !== undefined
+  const final_url = url_needs_fixing? "/" + sanitized_url : sanitized_url
   return {
-    type: GET_NEWS,
+    type: CRUD_NEWS,
     payload: {
       request: {
         method,
         headers,
         data,
-        url: `/news`
+        url: '/news' + final_url
       }
     }
   };
